0ed7d75564f5dbce56f8d549a19ce4d5772b0e96
massa <massafranck@gmail.com>
2018-06-26 15:58:59 +0200

    corrigé

---
+++ assets/js/fichier.js
@@ -0,0 +1,10 @@
+//déclaration de variable
+var text = document.getElementById('text');
+//déclaration de la fonction affichage
+function show(){
+  text.style.display = 'block';
+}
+//déclaration de la fontion cacher
+function hide(){
+  text.style.display = 'none';
+}
